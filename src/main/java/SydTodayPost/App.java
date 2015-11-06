0114c9513b6a9db8bed0d7e77de25b13bce7b028
neilmao <neil.xy.mao@gmail.com>
2015-11-07 10:45:25 +1100

    read configuration file from external file

--- src/main/java/SydTodayPost/App.java
+++ src/main/java/SydTodayPost/App.java
@@ -13,63 +13,73 @@ import java.util.Properties;
 public class App 
 {
     private static Log LOG = LogFactory.getLog(App.class);
-    private static final String SETTINGS_FILE = "/settings.properties";
+    private static final String SETTINGS_FILE = "settings.properties";
 
     public static void main( String[] args ) throws IOException {
 
         LOG.info("Loading configurations from file \"" + SETTINGS_FILE + "\"...");
-        Properties properties = new App().loadSettings();
 
-        if (args.length > 0 && properties != null)
-            properties.setProperty("thread", args[0]);
-
-        if (properties == null) {
+        Properties properties;
+        try {
+            properties = new App().loadSettings();
+        } catch (IOException ex) {
             LOG.warn("File \"" + SETTINGS_FILE + "\" is missing, program exit.");
-        } else {
-            Spider spider = new Spider(properties);
-            Thread thread = new Thread(spider);
+            createPropertyFile();
+            return;
+        }
 
-            String command = "Command: \"start\", \"stop\", \"status\", \"quit\"";
-            LOG.info(command);
+        Spider spider = new Spider(properties);
+        Thread thread = new Thread(spider);
 
-            // listen on keyboard
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String buffer;
-            while ((buffer = reader.readLine()) != null) {
-                if ("start".equals(buffer)) {
-                    if (!thread.isAlive())
-                        thread.start();
-                    else
-                        LOG.warn("Already started.");
-                    continue;
-                }
-                if ("status".equals(buffer)) {
-                    spider.reportStatus();
-                    continue;
-                }
-                if ("stop".equals(buffer)) {
-                    spider.stop();
-                    return;
-                }
-                if ("quit".equals(buffer) || "q".equals(buffer)) {
-                    if (thread.isAlive()) {
-                        spider.stop();
-                        thread.interrupt();
-                    }
-                    return;
-                }
-                LOG.warn("Unknown command. " + command);
+        String command = "Command: \"start\", \"stop\", \"status\", \"quit\"";
+        LOG.info(command);
+
+        // listen on keyboard
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String buffer;
+        while ((buffer = reader.readLine()) != null) {
+            if ("start".equals(buffer)) {
+                if (!thread.isAlive())
+                    thread.start();
+                else
+                    LOG.warn("Already started.");
+                continue;
             }
+            if ("status".equals(buffer)) {
+                spider.reportStatus();
+                continue;
+            }
+            if ("stop".equals(buffer)) {
+                spider.stop();
+                return;
+            }
+            if ("quit".equals(buffer) || "q".equals(buffer)) {
+                if (thread.isAlive()) {
+                    spider.stop();
+                    thread.interrupt();
+                }
+                return;
+            }
+            LOG.warn("Unknown command. " + command);
         }
     }
 
     private Properties loadSettings() throws IOException {
-        InputStream inputStream = getClass().getResourceAsStream(SETTINGS_FILE);
-        if (inputStream != null) {
-            Properties properties = new Properties();
-            properties.load(inputStream);
-            return properties;
-        }
-        return null;
+        InputStream inputStream = new FileInputStream(new File(SETTINGS_FILE));
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        return properties;
+    }
+
+    private static void createPropertyFile() throws IOException {
+        FileOutputStream fos = new FileOutputStream(new File(SETTINGS_FILE));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+        bw.write("username=\n");
+        bw.write("password=\n");
+        bw.write("# thread starts with http://\n");
+        bw.write("thread=\n");
+        bw.write("# delay every 15 mins to try if previous action received waiting\n");
+        bw.write("delay=900000\n");
+        bw.close();
     }
 }
